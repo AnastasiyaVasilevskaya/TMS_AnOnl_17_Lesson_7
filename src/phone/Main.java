0f6bc3b9f5261433a94cfd2676b1996a26f992b6
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-01-15 15:38:49 +0300

    task1. class Phone

--- src/phone/Main.java
+++ src/phone/Main.java
@@ -2,7 +2,20 @@ package phone;
 
 public class Main {
     public static void main(String[] args) {
+        Phone phone1 = new Phone("+375447276568", "Iphone 13", 200);
+        Phone phone2 = new Phone("+375297841244", "Samsung Galaxy");
+        Phone phone3 = new Phone();
 
+        System.out.println(phone1);
+        System.out.println(phone2);
+        System.out.println(phone3);
+
+        phone2.receiveCall("Дима", "+375333465366");
+
+        phone3.sendMessage("+375000000000", "+375001111111", "+375442222222");
+
+        System.out.println("Номер первого телефона " + phone1.getNumber());
     }
+
 
 }
